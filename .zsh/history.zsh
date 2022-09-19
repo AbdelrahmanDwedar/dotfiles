affd4a4cd731e1dc240847101a4ea4aeac66e866
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-09-20 00:39:35 +0200

    Add zsh history settings

---
+++ .zsh/history.zsh
@@ -0,0 +1,8 @@
+# don't put duplicate lines or lines starting with space in the history.
+HISTCONTROL=ignoreboth
+
+# for setting history length see HISTSIZE and HISTFILESIZE
+HISTFILE=~/.history
+HISTSIZE=1000
+SAVEHIST=1000
+setopt appendhistory
