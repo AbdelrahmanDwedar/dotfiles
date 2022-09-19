6358ed64b90e7a0c660299acc4d5bd0300eabf00
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-09-20 00:40:13 +0200

    Add zsh plugins managing file

---
+++ .zsh/plugins.zsh
@@ -0,0 +1,37 @@
+# Wakatime for terminal
+source ~/.zsh/plugins/wakatime/wakatime.plugin.zsh
+
+# Add zsh auto suggestions
+source ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# zsh syntax highlighting
+source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# config zsh completion
+fpath=(~/.zsh/plugins/zsh-completions/src $fpath)
+
+# zsh rust config extention
+source ~/.zsh/plugins/zsh-rust/zsh-rust.plugin.zsh
+
+# zsh history substring searing
+source ~/.zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
+source ~/.zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+
+# zsh auto notify
+source ~/.zsh/plugins/zsh-auto-notify/auto-notify.plugin.zsh
+
+# zsh web search
+source ~/.zsh/plugins/web-search/web_search.plugin.zsh
+
+# zsh copy-buffer (use ctrl+o to copy the current command before running it)
+source ~/.zsh/plugins/zsh-copybuffer/zsh-copybuffer.plugin.zsh
+
+# zsh dirhistory (alt+left to go to the last dir, alt+right to go to the new dir, alt+up to go to the root)
+source ~/.zsh/plugins/zsh-dirhistory/dirhistory.plugin.zsh
+
+# zsh check deps (get the way of installing dependencies)
+source ~/.zsh/plugins/check-deps/check-deps.plugin.zsh
+
+# history substring search keys
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
