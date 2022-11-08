6fdd675ca26ecaddf88c9efbe7d3a9f45939d039
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-11-08 13:52:04 +0200

    Add a script to set neovim or vim or vi as main editor

--- .zsh/vim.zsh
+++ .zsh/vim.zsh
@@ -6,3 +6,12 @@ _fix_cursor() {
 precmd_functions+=(_fix_cursor)
 
 # I made this as the box cursor bothers me all the time after using vim or neovim
+
+# set neovim or vim or vi as main editor
+if [[ -e $(which nvim) ]]; then
+	export EDITOR="$(which nvim)"
+elif [[ -e $(which vim) ]]; then
+	export EDITOR="$(which vim)"
+else
+	export EDITOR="$(which vi)"
+fi
