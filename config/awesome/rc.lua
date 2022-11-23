e19730a5ab52bb74b126bb527496fccf1969943c
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-11-24 01:48:17 +0200

    Update(awesome): Add powerline (inactivated)

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -18,6 +18,10 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
 
+-- powerline
+-- package.path = package.path .. '/usr/share/powerline/bindings/awesome/powerline.lua'
+-- require('powerline')
+
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
@@ -209,6 +213,8 @@ awful.screen.connect_for_each_screen(function(s)
         },
         s.mytasklist, -- Middle widget
         { -- Right widgets
+			-- powerline_widget,
+
             layout = wibox.layout.fixed.horizontal,
             mykeyboardlayout,
             wibox.widget.systray(),
