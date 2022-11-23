bf65b4b0b51b53d01af9c0d4656dea46fb615d1c
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-11-24 01:54:09 +0200

    Update(awesome): Set borders width to 0

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -452,7 +452,7 @@ root.keys(globalkeys)
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
-      properties = { border_width = beautiful.border_width,
+      properties = { border_width = 0 --[[ beautiful.border_width ]],
                      border_color = beautiful.border_normal,
                      focus = awful.client.focus.filter,
                      raise = true,
