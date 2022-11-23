fd15a58e23e8e1bad2ed215061c33e3051a0690c
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-11-24 01:53:25 +0200

    Update(awesome): Use modkey+shift+r instead of modkey+ctrl+r for refreshing awesome

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -282,7 +282,7 @@ globalkeys = gears.table.join(
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
               {description = "open a terminal", group = "launcher"}),
-    awful.key({ modkey, "Control" }, "r", awesome.restart,
+    awful.key({ modkey, "Shift" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
