c73262ca4fa979322fea7ec0e03575bc1e0527d7
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-12-25 15:45:42 +0200

    Feat(zsh): Add the corsur style for vi mode

--- .zsh/vim.zsh
+++ .zsh/vim.zsh
@@ -15,3 +15,35 @@ elif [[ -e $(which vim) ]]; then
 else
 	export EDITOR="$(which vi)"
 fi
+
+# vi mode 
+export VI_MODE_SET_CURSOR=true
+
+function _set_cursor() {
+    if [[ $TMUX = '' ]]; then
+      echo -ne $1
+    else
+      echo -ne "\ePtmux;\e\e$1\e\\"
+    fi
+}
+
+# Remove mode switching delay.
+KEYTIMEOUT=5
+
+function _set_block_cursor() { _set_cursor '\e[2 q' }
+function _set_beam_cursor() { _set_cursor '\e[0 q' }
+
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
+      _set_block_cursor
+  else
+      _set_beam_cursor
+  fi
+}
+zle -N zle-keymap-select
+# ensure beam cursor when starting new terminal
+precmd_functions+=(_set_beam_cursor) #
+# ensure insert mode and beam cursor when exiting vim
+zle-line-init() { zle -K viins; _set_beam_cursor }
+zle-line-finish() { _set_block_cursor }
+zle -N zle-line-finish
