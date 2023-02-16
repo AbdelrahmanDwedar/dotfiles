421b733a1219be2d6484d0d7382b71c1acb86872
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-02-17 00:41:38 +0200

    Fix(neovim): Fixed the nvim-tree issue of not starting with startup
    Added the autocmd to the lua/config/nvim-tree.lua to open the nvim-tree
    on startup everytime

--- config/nvim/lua/config/nvim-tree.lua
+++ config/nvim/lua/config/nvim-tree.lua
@@ -1,3 +1,11 @@
+local function open_nvim_tree()
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
 require('nvim-tree').setup({
 	sort_by = 'case_sensitive',
 	hijack_cursor = true,
