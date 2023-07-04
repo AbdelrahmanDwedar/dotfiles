e74f6056a5a09e869cd81c9f102c83010f32d327
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-07-04 15:54:12 +0200

    fix(neovim): fix issues with nvim cmp

--- config/nvim/lua/config/cmp.lua
+++ config/nvim/lua/config/cmp.lua
@@ -1,7 +1,5 @@
-local cmp_status_ok, cmp = pcall(require, 'cmp')
-if not cmp_status_ok then
-	return
-end
+local cmp = require('cmp')
+local nvim_lsp = require('lspconfig')
 
 local snip_status_ok, luasnip = pcall(require, 'luasnip')
 if not snip_status_ok then
@@ -47,11 +45,34 @@ local kind_icons = {
 
 cmp.setup({
 	snippet = {
+		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
-			luasnip.lsp_expand(args.body) -- For `luasnip` users.
+			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 		end,
 	},
-	mapping = {
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	formatting = {
+		fields = { 'kind', 'abbr', 'menu' },
+		format = function(entry, vim_item)
+			-- Kind icons
+			vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
+			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+			vim_item.menu = ({
+				nvim_lsp = '[LSP]',
+				luasnip = '[Snippet]',
+				buffer = '[File]',
+				path = '[Path]',
+			})[entry.source.name]
+			return vim_item
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
 		['<C-k>'] = cmp.mapping.select_prev_item(),
 		['<C-j>'] = cmp.mapping.select_next_item(),
 		['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-1), { 'i', 'c' }),
@@ -93,39 +114,73 @@ cmp.setup({
 			'i',
 			's',
 		}),
-	},
-	formatting = {
-		fields = { 'kind', 'abbr', 'menu' },
-		format = function(entry, vim_item)
-			-- Kind icons
-			vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
-			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-			vim_item.menu = ({
-				nvim_lsp = '[LSP]',
-				luasnip = '[Snippet]',
-				buffer = '[Buffer]',
-				path = '[Path]',
-			})[entry.source.name]
-			return vim_item
-		end,
-	},
-	sources = {
+	}),
+	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
-		{ name = 'luasnip' },
+		{ name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'vsnip' }, -- For vsnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+	}, {
 		{ name = 'buffer' },
 		{ name = 'path' },
-	},
-	confirm_opts = {
-		behavior = cmp.ConfirmBehavior.Replace,
-		select = false,
-	},
-	window = {
-		documentation = {
-			border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
-		},
-	},
+	}),
 	experimental = {
-		ghost_text = false,
-		native_menu = false,
+		ghost_text = true,
 	},
 })
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+	sources = cmp.config.sources({
+		{ name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+	}, {
+		{ name = 'buffer' },
+		{ name = 'path' },
+	}),
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' },
+	},
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' },
+	}, {
+		{ name = 'cmdline' },
+	}),
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+-- require('lspconfig')['<YOUR_LSP_SERVER>'].setup({
+-- 	capabilities = capabilities,
+-- })
+
+-- GoLang
+nvim_lsp['gopls'].setup{
+  cmd = {'gopls'},
+  -- on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    gopls = {
+      experimentalPostfixCompletions = true,
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+      },
+      staticcheck = true,
+    },
+  },
+  init_options = {
+    usePlaceholders = true,
+  }
+}
