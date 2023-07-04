bdd548689d068fa96404911f83dc1f4f72a94b19
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-07-04 15:56:42 +0200

    fix(neovim): update treesitter configs

--- config/nvim/lua/config/treesitter.lua
+++ config/nvim/lua/config/treesitter.lua
@@ -1,15 +1,13 @@
-local status_ok, configs = pcall(require, 'nvim-treesitter.configs')
-if not status_ok then
-	return
-end
+local configs = require('nvim-treesitter.configs')
 
 configs.setup({
 	ensure_installed = 'all', -- one of "all" or a list of languages
 	sync_install = false,
-	ignore_install = { '' }, -- List of parsers to ignore installing
+	auto_install = true,
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = { 'css' }, -- list of language that will be disabled
+		additional_vim_regex_highlighting = false,
 	},
 	autopairs = {
 		enable = true,
