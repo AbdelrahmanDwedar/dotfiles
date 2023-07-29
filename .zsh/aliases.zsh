adaa6d6a5f39dcfba1f48ed80df993bd542ca5cc
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-07-29 19:31:45 +0200

    fix(zsh): fix issues with the aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,67 +1,57 @@
-# Emacs
-if [[ -e ~/.emacs.d ]]; then
-  alias emacs="emacs -nw"
-fi
-
-# laravel => lv
-if [ -f $(which laravel) ]
+# NeoVim
+if [[ -e $(which nvim) ]]
 then
-  alias lv=laravel
+    alias nv=nvim
 fi
 
 # Python3 => py
 # pip3 => pip
 if [[ -e $(which python3) ]]
 then
-  alias py=python3
-  alias pip=pip3
+    alias py=python3
+    alias pip=pip3
 fi
 
 # flutter => fl
 if [ -e $(which flutter) ]
 then
-  alias fl=flutter
+    alias fl=flutter
 fi
 
 # ranger => ra
 if [[ -e $(which ranger) ]]
 then
-  alias ra=ranger
-  alias lr=ranger
+    alias ra=ranger
+    alias lr=ranger
 fi
 
 # neofetch
 if [[ -e $(which neofetch) ]]
 then
-  alias nf=neofetch
+    alias nf=neofetch
 fi
 
 # cpufetch
 if [[ -e $(which cpufetch) ]]
 then
-  alias cf=cpufetch
+    alias cf=cpufetch
 fi
 
 # lsd => ls
 if [ -e $(which lsd) ]; then
-  alias ls='lsd -F'
-  alias l='lsd -F'
-  alias la='lsd -AF'
-  alias ll='lsd -lF'
-  alias lt='lsd --tree'
-  alias lla='lsd -lFA'
-  alias lta='lsd --tree -A'
-  alias li='lsd -Fi'
-  alias lli='lsd -Fli'
-  alias llia='lsd -FAli'
+    alias ls='lsd -F'
+    alias l='lsd -F'
+    alias la='lsd -AF'
+    alias ll='lsd -lF'
+    alias lt='lsd --tree'
+    alias lla='lsd -lFA'
+    alias lta='lsd --tree -A'
+    alias li='lsd -Fi'
+    alias lli='lsd -Fli'
+    alias llia='lsd -FAli'
 fi
 
 # cd shortcuts
-# alias ..='cd ..'
-# alias ...="cd ../.."
-# alias ....="cd ../../.."
-# alias .....="cd ../../../.."
-# alias ......="cd ../../../../..."
 alias .1="cd .."
 alias .2="cd ../.."
 alias .3="cd ../../.."
@@ -74,9 +64,9 @@ alias mkdir="mkdir -p"
 # kitty kittens
 if [[ -e $(which kitty) ]]
 then
-  alias icat='kitty +kitten icat'
-  alias unicode='kitty +kitten unicode_input'
-  alias keys='kitty +kitten show_key'
+    alias icat='kitty +kitten icat'
+    alias unicode='kitty +kitten unicode_input'
+    alias keys='kitty +kitten show_key'
 fi
 
 # confirmation for rm mv cp commands
