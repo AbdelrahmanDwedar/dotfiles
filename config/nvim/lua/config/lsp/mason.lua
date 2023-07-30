decce99d3c209ded8a40628baa3f19a7b8634ade
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-07-30 21:31:43 +0200

    style(neovim): organize the null-ls configs

--- config/nvim/lua/config/lsp/mason.lua
+++ config/nvim/lua/config/lsp/mason.lua
@@ -6,7 +6,7 @@ local servers = {
 	'pyright',
 }
 
-local settings = {
+require('mason').setup({
 	ui = {
 		border = 'rounded',
 		icons = {
@@ -17,9 +17,7 @@ local settings = {
 	},
 	log_level = vim.log.levels.INFO,
 	max_concurrent_installers = 4,
-}
-
-require('mason').setup(settings)
+})
 require('mason-lspconfig').setup({
 	ensure_installed = servers,
 	automatic_installation = true,
@@ -27,20 +25,13 @@ require('mason-lspconfig').setup({
 
 local lspconfig = require('lspconfig')
 
-local opts = {}
-
 for _, server in pairs(servers) do
-	opts = {
+	local opts = {
 		on_attach = require('config.lsp.handlers').on_attach,
 		capabilities = require('config.lsp.handlers').capabilities,
 	}
 
 	server = vim.split(server, '@')[1]
-
-	local require_ok, conf_opts = pcall(require, 'user.lsp.settings.' .. server)
-	if require_ok then
-		opts = vim.tbl_deep_extend('force', conf_opts, opts)
-	end
 
 	lspconfig[server].setup(opts)
 end
