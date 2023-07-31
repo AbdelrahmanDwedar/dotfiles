3c352afd17b8381de109e6be5a425ad9b1a7024b
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-07-31 15:09:39 +0200

    fix(neovim): update the github theme with the new settings

--- config/nvim/lua/config/themes/github.lua
+++ config/nvim/lua/config/themes/github.lua
@@ -1,18 +1,20 @@
 require("github-theme").setup({
-  theme_style = "dark_default",
-  function_style = "italic",
-  sidebars = {"qf", "vista_kind", "terminal", "packer"},
-
-  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-  colors = {hint = "orange", error = "#ff0000"},
-
-  -- Overwrite the highlight groups
-  overrides = function(c)
-    return {
-      htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
-      DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
-      -- this will remove the highlight groups
-      TSField = {},
-    }
-  end
+	options = {
+		styles = {
+			functions = "italic"
+		},
+		darken = {
+			sidebars = {
+				list = {
+					{ "qf", "vista_kind", "terminal", "packer" }
+				}
+			}
+		}
+	},
+	specs = {
+		all = {
+			error = "#ff0000",
+			hint = "orange"
+		}
+	}
 })
