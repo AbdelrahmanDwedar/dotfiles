460eba8617d313181b3bebe74b4aff62b4f9d540
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-08-15 00:47:27 +0200

    feat(neovim): adjust the windows on opening neo-tree

--- config/nvim/lua/config/neotree.lua
+++ config/nvim/lua/config/neotree.lua
@@ -215,4 +215,22 @@ require('neo-tree').setup({
 			},
 		},
 	},
+	event_handlers = {
+		{
+			event = 'neo_tree_window_after_open',
+			handler = function(args)
+				if args.position == 'left' or args.position == 'right' then
+					vim.cmd('wincmd =')
+				end
+			end,
+		},
+		{
+			event = 'neo_tree_window_after_close',
+			handler = function(args)
+				if args.position == 'left' or args.position == 'right' then
+					vim.cmd('wincmd =')
+				end
+			end,
+		},
+	},
 })
