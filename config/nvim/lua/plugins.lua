17fbdde341960b19f02e8d0c1b82434a622483d7
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-08-15 01:55:06 +0200

    feat(neovim): add window picker support

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -80,11 +80,32 @@ return require('packer').startup(function()
 	-- neo-tree
 	use({
 		'nvim-neo-tree/neo-tree.nvim',
-		branch = 'v3.x',
 		requires = {
 			'nvim-lua/plenary.nvim',
 			'MunifTanjim/nui.nvim',
 			'nvim-tree/nvim-web-devicons',
+			{
+				-- only needed if you want to use the commands with "_with_window_picker" suffix
+				's1n7ax/nvim-window-picker',
+				tag = 'v1.*',
+				config = function()
+					require('window-picker').setup({
+						autoselect_one = true,
+						include_current = false,
+						filter_rules = {
+							-- filter using buffer options
+							bo = {
+								-- if the file type is one of following, the window will be ignored
+								filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+
+								-- if the buffer type is one of following, the window will be ignored
+								buftype = { 'terminal', 'quickfix' },
+							},
+						},
+						other_win_hl_color = '#e35e4f',
+					})
+				end,
+			},
 		},
 	})
 
