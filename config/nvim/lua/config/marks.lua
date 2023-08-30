38d4960f1b079ea72a4c58e1f2b6c0ffddd7a6a6
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-08-30 15:40:45 +0200

    fix(neovim): fix multicursor and marks.nvim for lazy loading

--- config/nvim/lua/config/marks.lua
+++ config/nvim/lua/config/marks.lua
@@ -1,16 +1,18 @@
-require('marks').setup({
-	default_mappings = true,
-	-- builtin_marks = { '.', '<', '>', '^', "'" },
-	builtin_marks = {},
-	cyclic = true, -- whether movements cycle back to the beginning/end of buffer. default true
-	force_write_shada = false, -- whether the shada file is updated after modifying uppercase marks. default false
-	refresh_interval = 250,
-	sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
-	excluded_filetypes = {}, -- disables mark tracking for specific filetypes. default {}
-	mappings = {},
+return function()
+	require('marks').setup({
+		default_mappings = true,
+		-- builtin_marks = { '.', '<', '>', '^', "'" },
+		builtin_marks = {},
+		cyclic = true, -- whether movements cycle back to the beginning/end of buffer. default true
+		force_write_shada = false, -- whether the shada file is updated after modifying uppercase marks. default false
+		refresh_interval = 250,
+		sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
+		excluded_filetypes = {}, -- disables mark tracking for specific filetypes. default {}
+		mappings = {},
 
-	-- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
-	-- sign/virttext. Bookmarks can be used to group together positions and quickly move
-	-- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
-	-- default virt_text is "".
-})
+		-- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
+		-- sign/virttext. Bookmarks can be used to group together positions and quickly move
+		-- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
+		-- default virt_text is "".
+	})
+end
--- config/nvim/lua/config/multicursor.lua
+++ config/nvim/lua/config/multicursor.lua
@@ -1,26 +1,28 @@
-local multi = require('multicursors')
-local N = require('multicursors.normal_mode')
+return function()
+	local multi = require('multicursors')
+	local N = require('multicursors.normal_mode')
 
-multi.setup({
-	normal_keys = {
-		['<C-/>'] = {
-			method = function()
-				require('multicursors.utils').call_on_selections(function(selection)
-					vim.api.nvim_win_set_cursor(0, { selection.row + 1, selection.col + 1 })
-					local line_count = selection.end_row - selection.row + 1
-					vim.cmd('normal ' .. line_count .. 'gcc')
-				end)
-			end,
-			opts = { desc = 'comment selections' },
+	multi.setup({
+		normal_keys = {
+			['<C-/>'] = {
+				method = function()
+					require('multicursors.utils').call_on_selections(function(selection)
+						vim.api.nvim_win_set_cursor(0, { selection.row + 1, selection.col + 1 })
+						local line_count = selection.end_row - selection.row + 1
+						vim.cmd('normal ' .. line_count .. 'gcc')
+					end)
+				end,
+				opts = { desc = 'comment selections' },
+			},
+			['<C-n>'] = {
+				method = N.find_next,
+				opts = { desc = 'Find next' },
+			},
+			['<C-N>'] = {
+				method = N.find_prev,
+				opts = { desc = 'Find prev' },
+			},
+			['<C-c>'] = { method = nil, opts = { desc = 'exit' } },
 		},
-		['<C-n>'] = {
-			method = N.find_next,
-			opts = { desc = 'Find next' },
-		},
-		['<C-N>'] = {
-			method = N.find_prev,
-			opts = { desc = 'Find prev' },
-		},
-		['<C-c>'] = { method = nil, opts = { desc = 'exit' } },
-	},
-})
+	})
+end
