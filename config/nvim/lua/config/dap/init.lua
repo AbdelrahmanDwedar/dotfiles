074f95179ff4797e868347a4e8b3011560a2b528
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-08-31 16:03:42 +0200

    style(neovim): fix styles

--- config/nvim/lua/config/dap/init.lua
+++ config/nvim/lua/config/dap/init.lua
@@ -2,14 +2,13 @@ require('dapui').setup()
 require('config.dap.mason')
 
 -- dap listener
-local dap, dapui = require("dap"), require("dapui")
-dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
+local dap, dapui = require('dap'), require('dapui')
+dap.listeners.after.event_initialized['dapui_config'] = function()
+	dapui.open()
 end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
+dap.listeners.before.event_terminated['dapui_config'] = function()
+	dapui.close()
 end
-dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
+dap.listeners.before.event_exited['dapui_config'] = function()
+	dapui.close()
 end
-
