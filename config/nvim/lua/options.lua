d9aba6e7dc353137d6c2d69b5aa0a5f747f74910
AbdelrahmanDwedar <dedou2005@gmail.com>
2024-04-19 21:12:25 +0200

    feat(neovim): add new options

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,4 +1,6 @@
 -- -==*==- Options -==*==- --
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- General
 vim.opt.clipboard = 'unnamedplus'
@@ -26,10 +28,18 @@ vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
 vim.opt.timeout = true
 vim.opt.timeoutlen = 1500
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.updatetime = 250
+vim.opt.inccommand = 'split'
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrw = 1
 
 -- styling
+vim.g.have_nerd_font = true
+vim.opt.showmode = false
+vim.opt.breakindent = true
 vim.opt.hlsearch = false
 vim.opt.hidden = false
 vim.opt.wrap = false
@@ -38,6 +48,7 @@ vim.opt.wildmode = 'full'
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.fileencoding = 'utf-8'
+vim.opt.list = true
 vim.opt.listchars = {
 	tab = ' ',
 	multispace = '󰧟',
@@ -45,4 +56,3 @@ vim.opt.listchars = {
 	extends = '󰇘',
 	precedes = '󰇘',
 }
-vim.opt.list = true
