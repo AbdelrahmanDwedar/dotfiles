d25199819c4968a35c9fe4170c7eef7810a3836c
AbdelrahmanDwedar <dedou2005@gmail.com>
2024-12-26 15:50:53 +0200

    fix(neovim): fix the todo comments plugin configs

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -305,6 +305,13 @@ require('lazy').setup({
 		},
 	},
 
+	{
+		'folke/trouble.nvim',
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = 'Trouble',
+	},
+
+
 	-- Auto-pairs
 	{
 		'windwp/nvim-autopairs',
@@ -391,10 +398,10 @@ require('lazy').setup({
 		end,
 	},
 
-	-- TODO: thing
 	{
 		'folke/todo-comments.nvim',
-		dependencies = { 'nvim-lua/plenary.nvim' },
+		dependencies = {
+			'nvim-lua/plenary.nvim', 'ibhagwan/fzf-lua' },
 		opts = {
 			signs = true, -- show icons in the signs column
 			sign_priority = 8, -- sign priority
@@ -403,14 +410,14 @@ require('lazy').setup({
 				FIX = {
 					icon = ' ', -- icon used for the sign, and in search results
 					color = 'error', -- can be a hex color, or a named color (see below)
-					alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE', '?' }, -- a set of other keywords that all map to this FIX keywords
+					alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE', '\\?' }, -- a set of other keywords that all map to this FIX keywords
 					-- signs = false, -- configure signs for some keywords individually
 				},
 				TODO = { icon = ' ', color = 'info' },
 				HACK = { icon = ' ', color = 'warning' },
-				WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', '!' } },
+				WARN = { icon = ' ', color = 'warning', alt = { 'WARNING' } },
 				PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
-				NOTE = { icon = ' ', color = 'hint', alt = { 'INFO', '*' } },
+				NOTE = { icon = ' ', color = 'hint', alt = { 'INFO', '\\*' } },
 				TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
 			},
 			gui_style = {
