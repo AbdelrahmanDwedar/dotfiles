36b3cd0050bbaae15393fa2e4139d78d340e8042
AbdelrahmanDwedar <dedou2005@gmail.com>
2025-01-09 21:26:30 +0200

    fix(neovim): remove bufferline items
    items caused the bufferline to crash as they are not working properly
    with filename

--- config/nvim/lua/config/bufferline.lua
+++ config/nvim/lua/config/bufferline.lua
@@ -114,31 +114,6 @@ return function()
 				options = {
 					toggle_hidden_on_enter = true, -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
 				},
-				items = {
-					{
-						name = 'Tests', -- Mandatory
-						highlight = { underline = true, sp = 'blue' }, -- Optional
-						priority = 2, -- determines where it will appear relative to other groups (Optional)
-						icon = '', -- Optional
-						matcher = function(buf) -- Mandatory
-							return buf.filename:match('%_test')
-								or buf.filename:match('%_spec')
-								or buf.filename:match('%.test')
-								or buf.filename:match('%.spec')
-						end,
-					},
-					{
-						name = 'Docs',
-						highlight = { undercurl = true, sp = 'green' },
-						auto_close = false, -- whether or not close this group if it doesn't contain the current buffer
-						matcher = function(buf)
-							return buf.filename:match('%.md') or buf.filename:match('%.txt')
-						end,
-						separator = { -- Optional
-							style = require('bufferline.groups').separator.tab,
-						},
-					},
-				},
 			},
 		},
 	})
