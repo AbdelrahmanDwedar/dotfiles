40dffcea2ea1f06166d8c4badb4ee0164f5ecb72
AbdelrahmanDwedar <dedou2005@gmail.com>
2025-01-09 21:40:24 +0200

    docs(neovim): add todo for the other plugins I made

--- config/nvim/lua/config/themes/init.lua
+++ config/nvim/lua/config/themes/init.lua
@@ -1,9 +1,11 @@
 require('config.themes.catppuccin')
 require('config.themes.dracula')
-require('config.themes.github')
+-- TODO: github config in the plugin is outdated
+-- require('config.themes.github')
 require('config.themes.nightfox')
 require('config.themes.nord')
 require('config.themes.onedark')
 require('config.themes.onenord')
-require('config.themes.rose-pine')
+-- TODO: get this to work in the plugin first
+-- require('config.themes.rose-pine')
 require('config.themes.tokyonight')
