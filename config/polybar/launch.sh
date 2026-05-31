b26e48faea4c86a27938f38522ca912147884286
AbdelrahmanDwedar <dedou2005@gmail.com>
2026-06-01 01:30:06 +0300

    style(polybar): switch to floating island design with multiple bars

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
 polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar mybar 2>&1 | tee -a /tmp/polybar2.log & disown
+# Launch the three islands
+echo "---" | tee -a /tmp/polybar_left.log /tmp/polybar_center.log /tmp/polybar_right.log
+polybar left 2>&1 | tee -a /tmp/polybar_left.log & disown
+polybar center 2>&1 | tee -a /tmp/polybar_center.log & disown
+polybar right 2>&1 | tee -a /tmp/polybar_right.log & disown
 
 echo "Bars launched..."
